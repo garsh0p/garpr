9dea12f79e2ecce475169d332787e469a279124d
Ivan <garsh0p@gmail.com>
2014-07-15 18:28:10 -0700

    use raw for challonge and tests

--- scraper/challonge.py
+++ scraper/challonge.py
@@ -22,42 +22,36 @@ class ChallongeScraper(object):
 
         self.api_key_dict = {'api_key': self.api_key}
 
+        self.raw_dict = None
+        self.get_raw()
+
     def get_raw(self):
-        raw_dict = {}
+        if self.raw_dict == None:
+            self.raw_dict = {}
 
-        url = TOURNAMENT_URL % self.tournament_id
-        raw_dict['tournament'] = self._check_for_200(requests.get(url, params=self.api_key_dict)).json()
+            url = TOURNAMENT_URL % self.tournament_id
+            self.raw_dict['tournament'] = self._check_for_200(requests.get(url, params=self.api_key_dict)).json()
 
-        url = MATCHES_URL % self.tournament_id
-        raw_dict['matches'] = self._check_for_200(requests.get(url, params=self.api_key_dict)).json()
+            url = MATCHES_URL % self.tournament_id
+            self.raw_dict['matches'] = self._check_for_200(requests.get(url, params=self.api_key_dict)).json()
 
-        url = PARTICIPANTS_URL % self.tournament_id
-        raw_dict['participants'] = self._check_for_200(requests.get(url, params=self.api_key_dict)).json()
+            url = PARTICIPANTS_URL % self.tournament_id
+            self.raw_dict['participants'] = self._check_for_200(requests.get(url, params=self.api_key_dict)).json()
 
-        return raw_dict
+        return self.raw_dict
 
     def get_name(self):
-        url = TOURNAMENT_URL % self.tournament_id
-        json_response = requests.get(url, params=self.api_key_dict).json()
-        return json_response['tournament']['name'].strip()
+        return self.get_raw()['tournament']['tournament']['name'].strip()
 
     def get_date(self):
-        url = TOURNAMENT_URL % self.tournament_id
-        json_response = requests.get(url, params=self.api_key_dict).json()
-        return iso8601.parse_date(json_response['tournament']['created_at'])
+        return iso8601.parse_date(self.get_raw()['tournament']['tournament']['created_at'])
 
     def get_matches(self):
-        matches_url = MATCHES_URL % self.tournament_id
-        matches_json_response = requests.get(matches_url, params=self.api_key_dict).json()
-
-        participants_url = PARTICIPANTS_URL % self.tournament_id
-        participants_json_response = requests.get(participants_url, params=self.api_key_dict).json()
-
         player_map = dict((p['participant']['id'], p['participant']['name']) 
-                          for p in participants_json_response)
+                          for p in self.get_raw()['participants'])
 
         matches = []
-        for m in matches_json_response:
+        for m in self.get_raw()['matches']:
             m = m['match']
             winner_id = m['winner_id']
             loser_id = m['loser_id']
@@ -69,11 +63,7 @@ class ChallongeScraper(object):
         return matches
 
     def get_players(self):
-        # participant
-        url = PARTICIPANTS_URL % self.tournament_id
-        json_response = requests.get(url, params=self.api_key_dict).json()
-
-        return [p['participant']['name'] for p in json_response]
+        return [p['participant']['name'] for p in self.get_raw()['participants']]
 
     def _check_for_200(self, response):
         if response.status_code != 200:
--- test/int_tests/int_test_challonge.py
+++ test/int_tests/int_test_challonge.py
@@ -1,12 +1,12 @@
 from scraper.challonge import ChallongeScraper
 import unittest
+import iso8601
 
 TOURNAMENT_ID = "TNE_Singles"
+scraper = ChallongeScraper(TOURNAMENT_ID)
 
 class IntTestChallongeScraper(unittest.TestCase):
-    def test_challonge(self):
-        scraper = ChallongeScraper(TOURNAMENT_ID)
-
+    def test_get_raw(self):
         raw = scraper.get_raw()
         self.assertTrue('tournament' in raw)
         self.assertTrue('matches' in raw)
@@ -15,3 +15,15 @@ class IntTestChallongeScraper(unittest.TestCase):
         self.assertTrue('tournament' in raw['tournament'])
         self.assertEquals(len(raw['matches']), 126)
         self.assertEquals(len(raw['participants']), 64)
+
+    def test_get_name(self):
+        self.assertEquals(scraper.get_name(), "The Next Episode")
+
+    def test_get_date(self):
+        self.assertEquals(scraper.get_date(), iso8601.parse_date("2014-03-23T17:28:34-04:00"))
+
+    def test_get_matches(self):
+        self.assertEquals(len(scraper.get_matches()), 126)
+
+    def test_get_players(self):
+        self.assertEquals(len(scraper.get_players()), 64)
