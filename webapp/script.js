41b6057093714e0958cbda309453d63925f92f41
Ivan <garsh0p@gmail.com>
2014-09-15 21:28:18 -0700

    add player/tournament tabs

--- webapp/script.js
+++ webapp/script.js
@@ -1,30 +1,14 @@
 var app = angular.module('myApp', ['ngRoute']);
 
 app.service('RegionService', function ($http) {
-    var regions = [];
-    var region = '';
-
     var service = {
-        getRegion: function() {
-            return region;
-        },
-
-        getRegions: function() {
-            return regions;
-        },
-
-        setRegion: function(r) {
-            region = r;
-        },
-
-        setRegions: function(r) {
-            regions = r;
-        }
+        regions: [],
+        region: ''
     };
 
     $http.get('http://garsh0p.no-ip.biz:5100/regions').
         success(function(data) {
-            service.setRegions(data.regions);
+            service.regions = data.regions;
         });
 
     return service;
@@ -34,21 +18,51 @@ app.service('RegionService', function ($http) {
 app.config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/:region/rankings', {
         templateUrl: 'rankings.html',
-        controller: 'RankingsController'
+        controller: 'RankingsController',
+        activeTab: 'rankings'
+    }).
+    when('/:region/players', {
+        templateUrl: 'players.html',
+        controller: 'PlayersController',
+        activeTab: 'players'
+    }).
+    when('/:region/tournaments', {
+        templateUrl: 'tournaments.html',
+        controller: 'TournamentsController',
+        activeTab: 'tournaments'
     }).
     otherwise({
         redirectTo: '/norcal/rankings'
     });
 }]);
 
-app.controller("RegionDropdownController", function($scope, RegionService) {
+app.controller("RegionDropdownController", function($scope, $route, RegionService) {
     $scope.regionService = RegionService;
+    $scope.$route = $route;
 });
 
 app.controller("RankingsController", function($scope, $http, $routeParams, RegionService) {
-    RegionService.setRegion($routeParams.region);
+    RegionService.region = $routeParams.region;
 
     $http.get('http://garsh0p.no-ip.biz:5100/' + $routeParams.region + '/rankings').
+        success(function(data) {
+            $scope.data = data;
+        });
+});
+
+app.controller("TournamentsController", function($scope, $http, $routeParams, RegionService) {
+    RegionService.region = $routeParams.region;
+
+    $http.get('http://garsh0p.no-ip.biz:5100/' + $routeParams.region + '/tournaments').
+        success(function(data) {
+            $scope.data = data;
+        });
+});
+
+app.controller("PlayersController", function($scope, $http, $routeParams, RegionService) {
+    RegionService.region = $routeParams.region;
+
+    $http.get('http://garsh0p.no-ip.biz:5100/' + $routeParams.region + '/players').
         success(function(data) {
             $scope.data = data;
         });
