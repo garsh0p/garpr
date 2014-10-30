9bd4b1e5add821b215431b7a459965cb257c2f6f
Ivan <garsh0p@gmail.com>
2014-10-29 19:14:01 -0700

    test_challonge wip

--- test/test_scraper/test_challonge.py
+++ test/test_scraper/test_challonge.py
@@ -1,5 +1,6 @@
 import unittest
-import mock
+from mock import patch
+import scraper.challonge
 from scraper.challonge import ChallongeScraper
 
 URL = "http://www.fakeurl.com/asdf"
@@ -7,8 +8,11 @@ FILE_TEMPLATE = "test/scraper/data/challonge_%d.html"
 NUM_FILES = 9
 
 class TestChallongeScraper(unittest.TestCase):
+    @patch('scraper.challonge.CHALLONGE_API_KEY_PATH', 'asdf')
     def setUp(self):
+        print scraper.challonge.CHALLONGE_API_KEY_PATH
         self.scraper = ChallongeScraper(URL)
         self.files = self.load_all_files()
 
-    # TODO add tests working with raw
+    def test_asdf(self):
+        pass
