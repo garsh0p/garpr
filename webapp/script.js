b122259d575b74e3de76110340627762b2c31fc8
Ivan <garsh0p@gmail.com>
2014-11-02 01:56:52 -0700

    refactor webapp to use services

--- webapp/script.js
+++ webapp/script.js
@@ -1,13 +1,32 @@
 var app = angular.module('myApp', ['ngRoute']);
 
-var toTitleCase = function(str){
-    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
-}
-
-app.service('RegionService', function ($http) {
+app.service('RegionService', function ($http, PlayerService, TournamentService, RankingsService) {
     var service = {
         regions: [],
-        region: ''
+        region: '',
+        setRegion: function (newRegion) {
+            if (newRegion != this.region) {
+                this.region = newRegion;
+                PlayerService.playerList = null;
+                TournamentService.tournamentList = null;
+                RankingsService.rankingsList = null;
+
+                $http.get('http://garsh0p.no-ip.biz:5100/' + this.region + '/players').
+                    success(function(data) {
+                        PlayerService.playerList = data;
+                    });
+
+                $http.get('http://garsh0p.no-ip.biz:5100/' + this.region + '/tournaments').
+                    success(function(data) {
+                        TournamentService.tournamentList = data;
+                    });
+
+                $http.get('http://garsh0p.no-ip.biz:5100/' + this.region + '/rankings').
+                    success(function(data) {
+                        RankingsService.rankingsList = data;
+                    });
+            }
+        }
     };
 
     $http.get('http://garsh0p.no-ip.biz:5100/regions').
@@ -18,6 +37,26 @@ app.service('RegionService', function ($http) {
     return service;
 });
 
+app.service('PlayerService', function ($http) {
+    var service = {
+        playerList: null
+    };
+    return service;
+});
+
+app.service('TournamentService', function ($http) {
+    var service = {
+        tournamentList: null
+    };
+    return service;
+});
+
+app.service('RankingsService', function ($http) {
+    var service = {
+        rankingsList: null
+    };
+    return service;
+});
 
 app.config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/:region/rankings', {
@@ -54,39 +93,27 @@ app.controller("RegionDropdownController", function($scope, $route, RegionServic
     $scope.$route = $route;
 });
 
-app.controller("RankingsController", function($scope, $http, $routeParams, RegionService) {
-    RegionService.region = $routeParams.region;
-    $scope.region = $routeParams.region;
-
-    $http.get('http://garsh0p.no-ip.biz:5100/' + $routeParams.region + '/rankings').
-        success(function(data) {
-            $scope.data = data;
-        });
+app.controller("RankingsController", function($scope, $http, $routeParams, RegionService, RankingsService) {
+    RegionService.setRegion($routeParams.region);
+    $scope.regionService = RegionService;
+    $scope.rankingsService = RankingsService
 });
 
-app.controller("TournamentsController", function($scope, $http, $routeParams, RegionService) {
-    RegionService.region = $routeParams.region;
-
-    $http.get('http://garsh0p.no-ip.biz:5100/' + $routeParams.region + '/tournaments').
-        success(function(data) {
-            $scope.data = data;
-        });
+app.controller("TournamentsController", function($scope, $http, $routeParams, RegionService, TournamentService) {
+    RegionService.setRegion($routeParams.region);
+    $scope.regionService = RegionService;
+    $scope.tournamentService = TournamentService;
 });
 
-app.controller("PlayersController", function($scope, $http, $routeParams, RegionService) {
-    RegionService.region = $routeParams.region;
-    $scope.region = $routeParams.region;
-
-    $http.get('http://garsh0p.no-ip.biz:5100/' + $routeParams.region + '/players').
-        success(function(data) {
-            $scope.data = data;
-        });
+app.controller("PlayersController", function($scope, $http, $routeParams, RegionService, PlayerService) {
+    RegionService.setRegion($routeParams.region);
+    $scope.regionService = RegionService;
+    $scope.playerService = PlayerService;
 });
 
 app.controller("PlayerDetailController", function($scope, $http, $routeParams, RegionService) {
-    RegionService.region = $routeParams.region;
-    $scope.region = $routeParams.region;
-    $scope.prettyRegionName = toTitleCase($scope.region);
+    RegionService.setRegion($routeParams.region);
+    $scope.regionService = RegionService;
     $scope.playerId = $routeParams.playerId;
 
     $http.get('http://garsh0p.no-ip.biz:5100/' + $routeParams.region + '/players/' + $routeParams.playerId).
@@ -94,9 +121,9 @@ app.controller("PlayerDetailController", function($scope, $http, $routeParams, R
             $scope.playerData = data;
         });
 
-    $http.get('http://garsh0p.no-ip.biz:5100/' + $routeParams.region + '/matches?player=' + $routeParams.playerId).
+    $http.get('http://garsh0p.no-ip.biz:5100/' + $routeParams.region + '/matches/' + $routeParams.playerId).
         success(function(data) {
-            $scope.matches = data;
+            $scope.matches = data.matches.reverse();
         });
 
 });
