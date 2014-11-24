ba21138e4eea7e189e1f74a5741287dbb4058860
Ivan <garsh0p@gmail.com>
2014-11-24 01:07:10 -0800

    fix import script

--- scripts/import_tournament.py
+++ scripts/import_tournament.py
@@ -7,8 +7,9 @@ import rankings
 from ConfigParser import ConfigParser
 from pymongo import MongoClient
 import getpass
+from bson.objectid import ObjectId
 
-DEFAULT_RATING = TrueskillRating()
+DEFAULT_RATING = {}
 
 def parse_config():
     config = ConfigParser()
@@ -39,10 +40,10 @@ def import_tournament(type, path, bracket, region, name):
 
     dao = Dao(region, mongo_client=mongo_client)
 
-    import_players(scraper, dao)
+    player_map = get_player_alias_to_id_map(scraper, dao)
 
     # TODO pass in a map of overrides for specific players
-    tournament = Tournament.from_scraper(type, scraper, dao)
+    tournament = Tournament.from_scraper(type, scraper, player_map, region)
     if name:
         tournament.name = name
 
@@ -53,50 +54,58 @@ def import_tournament(type, path, bracket, region, name):
 
     click.echo("Done!")
 
-def import_players(scraper, dao):
-    for player in scraper.get_players():
-        db_player = dao.get_player_by_alias(player)
-        if db_player == None:
-            click.echo("%s does not exist in the current region %s." % (player, dao.region_id))
+def get_player_alias_to_id_map(scraper, dao):
+    player_map = dao.get_player_id_map_from_player_aliases(scraper.get_players())
 
-            db_player_list = dao.get_players_by_alias_from_all_regions(player)
-            if not db_player_list:
-                click.echo("%s could not be found in any region." % player)
+    for alias, id in player_map.iteritems():
+        if id is None:
+            click.echo("%s does not exist in the current region %s." % (alias, dao.region_id))
 
-                add_new = click.confirm("Add this player as a new player?", default=True)
-                if add_new:
-                    name = click.prompt("Enter name", default=player)
-                    alias_set = set()
-                    alias_set.add(name.lower())
-                    alias_set.add(player.lower())
+            db_player_list = dao.get_players_by_alias_from_all_regions(alias)
 
-                    db_player = dao.get_player_by_alias(name)
-                    if db_player:
-                        click.echo("%s already exists, adding %s as an alias." % (name, player))
-                        dao.add_alias_to_player(db_player, player)
-                        continue
+            # print out alias matches from other regions as suggestions list
+            if db_player_list:
+                for db_player in db_player_list:
+                    click.echo(str(db_player))
 
-                    player_to_add = Player(name, list(alias_set), DEFAULT_RATING, False)
-                    dao.add_player(player_to_add)
-                else:
-                    player_to_add_alias_to = None
-                    while not player_to_add_alias_to:
-                        alias = click.prompt("Enter a player who is referred to by this alias")
-                        player_to_add_alias_to = dao.get_player_by_alias(alias)
+            # prompt to add as new player or add alias to existing player
+            add_new = click.confirm("Add this player as a new player?", default=True)
+            if add_new:
+                name = click.prompt("Enter name", default=alias)
+                alias_set = set()
+                alias_set.add(name.lower())
+                alias_set.add(alias.lower())
 
-                    dao.add_alias_to_player(player_to_add_alias_to, player)
+                db_player = dao.get_player_by_alias(name)
+                if db_player:
+                    click.echo("%s already exists, adding %s as an alias." % (name, player))
+                    dao.add_alias_to_player(db_player, player)
+                    continue
+
+                regions = []
+                include_in_region = click.confirm("Associate with current region %s?" % dao.region_id, default=True)
+                if include_in_region:
+                    regions.append(dao.region_id)
+
+                player_to_add = Player(name, list(alias_set), DEFAULT_RATING, regions)
+                click.echo('Inserting player: %s' % player_to_add)
+                new_player_id = dao.insert_player(player_to_add)
+                player_map[alias] = new_player_id
             else:
-                click.echo("Found the following players matching alias %s:" % player)
-                for p in db_player_list:
-                    click.echo(p)
-                
-                id = click.prompt("Enter the ID of this player or 'none' to add as a new player")
-                if id == 'none':
-                    # TODO new player
-                else:
+                player_id_to_use = click.prompt("Enter this player's ID")
 
-        else:
-            click.echo("Found player: %s" % db_player)
+                db_player = dao.get_player_by_id(ObjectId(player_id_to_use))
+                if db_player is None:
+                    raise Exception('Player with id % not found!' % player_id_to_use)
+
+                if not alias.lower() in db_player.aliases:
+                    add_alias = click.confirm("Add %s as a new alias for player %s?" % (alias, db_player.name), default=True)
+                    if add_alias:
+                        dao.add_alias_to_player(player_to_add_alias_to, alias)
+
+                player_map[alias] = db_player.id
+
+    return player_map
 
 if __name__ == '__main__':
     import_tournament()
