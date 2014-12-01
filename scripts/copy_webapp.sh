280e1c9c817392f1f66165f311e07c25df1b99ba
Ivan Van <garsh0p@gmail.com>
2014-11-30 20:14:45 -0500

    update deploy script

--- scripts/copy_webapp.sh
+++ scripts/copy_webapp.sh
@@ -1,4 +1,5 @@
-rm -rf /var/www/*
-mkdir /var/www/api
-cp -r webapp /var/www
-cp garpr.wsgi /var/www/api
+sudo rm -rf /var/www/*
+sudo mkdir /var/www/api
+sudo cp -r webapp /var/www
+sudo cp garpr.wsgi /var/www/api
+sudo /etc/init.d/apache2 restart
