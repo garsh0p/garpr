d8b1f598f400a0d0fb02a99fe9cfc76b084f828a
jhertz <hectohertz@gmail.com>
2015-11-16 00:48:50 -0500

    enhanced startup script

--- start.sh
+++ start.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 if [ -z "$VIRTUAL_ENV" ] 
 	then
+		echo "activating venv"
 		source ./env/bin/activate
 fi
-python server.py 3000 True &
-pushd webapp; python -m SimpleHTTPServer; popd
+if [[ "$(ps aux | grep server.py)" == *"python server.py"* ]]
+	then
+		echo "backend is already running"
+	else
+		python server.py 3000 True &
+fi
+if [[ "$(ps aux | grep SimpleHTTPServer)" == *"python -m SimpleHTTPServer"* ]]
+	then
+		echo "frontend is already running"
+	else
+		pushd webapp; python -m SimpleHTTPServer &; popd
+echo "everything started, try http://localhost:8000"
