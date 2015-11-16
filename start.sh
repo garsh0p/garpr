a56e2b4cf202ae0cc146f3b8262f7100bcd98b3b
jhertz <hectohertz@gmail.com>
2015-11-16 01:08:03 -0500

    more deployment scripts

--- start.sh
+++ start.sh
@@ -3,16 +3,28 @@ if [ -z "$VIRTUAL_ENV" ]
 	then
 		echo "activating venv"
 		source ./env/bin/activate
+	else
+		echo "venv already active"
+fi
+if [[ "$(ps aux | grep mongo)" == *"mongod"* ]]
+	then
+		echo "mongod is already running"
+	else
+		echo "starting mongo"
+		mongod &
 fi
 if [[ "$(ps aux | grep server.py)" == *"python server.py"* ]]
 	then
 		echo "backend is already running"
 	else
+		echo "starting backend"
 		python server.py 3000 True &
 fi
 if [[ "$(ps aux | grep SimpleHTTPServer)" == *"python -m SimpleHTTPServer"* ]]
 	then
 		echo "frontend is already running"
 	else
-		pushd webapp; python -m SimpleHTTPServer &; popd
+		echo "starting frontend"
+		pushd webapp; python -m SimpleHTTPServer ; popd &
+fi
 echo "everything started, try http://localhost:8000"
---
+++ stop.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "assuming you don't want mongod killed"
+killall python
+# TODO: make this suck less LMAO
