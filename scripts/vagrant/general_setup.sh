81385f2f30c0767961f02f229e2ecc356bf0f73b
Adam Coulon <adamgcoulon@gmail.com>
2016-06-01 19:49:37 -0500

    removed setup of python virtualenv within the vagrant VM

--- scripts/vagrant/general_setup.sh
+++ scripts/vagrant/general_setup.sh
@@ -10,7 +10,7 @@ echo "Installing GIT..."
 sudo apt-get install -y git
 
 echo "Installing Python tools..."
-sudo apt-get install -y python-dev libxml2-dev libxslt1-dev zlib1g-dev python-virtualenv
+sudo apt-get install -y python-dev python-pip libxml2-dev libxslt1-dev zlib1g-dev
 
 echo "rsyncing default Vagrant synced folder to vagrant user home..."
 rsync -a /vagrant/ /home/vagrant
