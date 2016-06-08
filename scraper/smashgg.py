46472e9f65754ae0853ff2d696bcafc8bd7cd30f
Cooke, Brandon (bc719c) <bc719c@us.att.com>
2016-06-07 23:56:04 -0400

    Added method to get matches. Reformatted test structure to that of unit testing.

--- scraper/smashgg.py
+++ scraper/smashgg.py
@@ -31,18 +31,33 @@ class SmashGGScraper(object):
 
     def get_matches(self):
         matches = []
-
+        sets = self.get_raw()['smashgg']['entities']['sets']
+        for set in sets:
+            winner_id = set['winnerId']
+            loser_id = set['loserId']
+            match = MatchResult(winner_id, loser_id)
+            matches.append(match)
         return matches
 
     def get_player_by_id(self, id):
         if self.players is None or len(self.players) == 0:
-            self.get_players(self.tournament_id)
+            self.get_players()
 
         for player in self.players:
             if id == int(player.smashgg_id):
                 return player
 
     def get_players(self):
+        if self.players is None or len(self.players) == 0:
+            self.get_smashgg_players()
+
+        tags = []
+        for player in self.players:
+            tags.append(str(player.smash_tag).strip())
+        return tags
+
+
+    def get_smashgg_players(self):
         self.players = []
         seeds = self.get_raw()['smashgg']['entities']['seeds']
         for seed in seeds:
@@ -55,15 +70,19 @@ class SmashGGScraper(object):
                 name = this_player[id]['name'].strip()
                 region = this_player[id]['region'].strip()
             except:
-                print 'Data for player ' + id + ' not found'
+                print self.log('Data for player ' + id + ' not found')
                 continue
 
             player = SmashGGPlayer(id, name, tag, region)
             self.players.append(player)
+        return self.players
 
     def _check_for_200(self, response):
         response.raise_for_status()
         return response
+
+    def log(self, msg):
+        return "    [SmashGG] " + msg
 
 class SmashGGPlayer(object):
     def __init__(self, smashgg_id, name, smash_tag, region):
--- test/test_scraper/test_smashgg.py
+++ test/test_scraper/test_smashgg.py
@@ -6,19 +6,30 @@ import scraper.smashgg
 from mock import patch, Mock
 from datetime import datetime
 from model import MatchResult
+from scraper.smashgg import SmashGGPlayer
 from scraper.smashgg import SmashGGScraper
 
 TEST_TOURNAMENT_ID = 11226
 TEST_PLAYER_ID = 13932
 
 class TestSmashGGScraper(unittest.TestCase):
-    '''def test_get_raw(self):'''
+    def setUp(self):
+        self.tournament = SmashGGScraper(TEST_TOURNAMENT_ID)
 
     def test_tournament_creation(self):
-        tournament = SmashGGScraper(TEST_TOURNAMENT_ID)
-        players = tournament.get_players()
-        player = tournament.get_player_by_id(TEST_PLAYER_ID)
+        players = self.tournament.get_smashgg_players()
+        player = self.tournament.get_player_by_id(TEST_PLAYER_ID)
+        tags = self.tournament.get_players()
 
+    def test_get_player_by_id(self):
+        player = self.tournament.get_player_by_id(TEST_PLAYER_ID)
+        self.assertEqual(player.name, 'Jose Angel Aldama')
+        self.assertEqual(player.smash_tag, 'Lucky')
+        self.assertEqual(player.region, 'SoCal')
 
+    def test_get_players(self):
+        self.assertEqual(len(self.tournament.get_players()), 29)
 
+    def test_get_matches(self):
+        self.assertEqual(len(self.tournament.get_matches()), 47)
 
