3f5275b5c037ec519fc51faba88344f72b2e3d50
Cooke, Brandon (bc719c) <bc719c@us.att.com>
2016-06-18 09:48:13 -0400

    Revert "Added Logging error handling to the SmashGG Module"
    This reverts commit bbe5e3bf8d28ac853c2df121a5533426986efc22.

--- scraper/smashgg.py
+++ scraper/smashgg.py
@@ -1,7 +1,6 @@
-import os
 import requests
+import os
 from model import MatchResult
-from garprLogging.log import Log
 
 BASE_SMASHGG_API_URL = "https://api.smash.gg/phase_group/"
 TOURNAMENT_URL = os.path.join(BASE_SMASHGG_API_URL, '%s')
@@ -62,8 +61,7 @@ class SmashGGScraper(object):
                 match = MatchResult(winner.smash_tag, loser.smash_tag)
                 matches.append(match)
         except Exception as ex:
-            msg = 'An error occurred in the retrieval of matches from SmashGG: ' + str(ex)
-            Log.log('SmashGG', msg)
+            msg = 'An error occured in the retrieval of matches: ' + str(ex)
         return matches
 
     def get_player_by_entrant_id(self, id):
@@ -71,51 +69,35 @@ class SmashGGScraper(object):
         :param id: id of the entrant for the current tournament
         :return: a SmashGGPlayer object that belongs to the given tournament entrant number
         """
-        player = None
-        try:
-            if self.players is None or len(self.players) == 0:
-                self.get_smashgg_players()
+        if self.players is None or len(self.players) == 0:
+            self.get_smashgg_players()
 
-            for player in self.players:
-                if id == int(player.entrant_id):
-                    return player
-        except Exception as ex:
-            msg = 'An error occurred in the retrieval of SmashGG Entrant ' + id + ': ' + str(ex.message)
-            Log.log('SmashGG', msg)
-        return player
+        for player in self.players:
+            if id == int(player.entrant_id):
+                return player
 
     def get_player_by_smashgg_id(self, id):
         """
         :param id: id of the smashGG  player's account
         :return: a SmashGGPlayer object that belongs to the given smashgg id number
         """
-        player = None
-        try:
-            if self.players is None or len(self.players) == 0:
-                self.get_smashgg_players()
+        if self.players is None or len(self.players) == 0:
+            self.get_smashgg_players()
 
-            for player in self.players:
-                if id == int(player.smashgg_id):
-                    return player
-        except Exception as ex:
-            msg = 'An error occurred in the retrieval of SmashGG Player ' + id + ': ' + str(ex.message)
-            Log.log('SmashGG', msg)
-        return player
+        for player in self.players:
+            if id == int(player.smashgg_id):
+                return player
 
     def get_players(self):
         """
         :return: the smash tags of every player who is in the given bracket
         """
-        tags = []
-        try:
-            if self.players is None or len(self.players) == 0:
-                self.get_smashgg_players()
+        if self.players is None or len(self.players) == 0:
+            self.get_smashgg_players()
 
-            for player in self.players:
-                tags.append(str(player.smash_tag).strip())
-        except Exception as ex:
-            msg = 'An error occurred in the retrieval of players from SmashGG: ' + str(ex.message)
-            Log.log('SmashGG', msg)
+        tags = []
+        for player in self.players:
+            tags.append(str(player.smash_tag).strip())
         return tags
 
     def get_smashgg_players(self):
@@ -125,63 +107,59 @@ class SmashGGScraper(object):
         tournament entrant id, and overall smashgg id
         """
         self.players = []
-        try:
-            seeds = self.get_raw()['smashgg']['entities']['seeds']
-            for seed in seeds:
-                tag = None
+        seeds = self.get_raw()['smashgg']['entities']['seeds']
+        for seed in seeds:
+            tag = None
+            name = None
+            state = None
+            country = None
+            region = None
+
+            #ACCESS THE PLAYERS IN THE JSON AND EXTRACT THE SMASHTAG
+            #IF NO SMASHTAG, WE SHOULD SKIP TO THE NEXT ITERATION
+            entrant_id = seed['entrantId']
+            this_player = seed['mutations']['players']
+            for player_id in this_player:
+                id = player_id
+
+            try:
+                tag = this_player[id]['gamerTag'].strip()
+            except:
+                print self.log('Player for id ' + str(id) + ' not found')
+                continue
+
+            #EXTRACT EXTRA DATA FROM SMASHGG WE MAY WANT TO USE LATER
+            #ENCAPSULATE IN A SMASHGG SPECIFIC MODEL
+            try:
+                name = this_player[id]['name'].strip()
+            except Exception as e:
                 name = None
-                state = None
-                country = None
-                region = None
+                print self.log('SmashGGPlayer ' + tag + ': name | ' + str(e))
 
-                #ACCESS THE PLAYERS IN THE JSON AND EXTRACT THE SMASHTAG
-                #IF NO SMASHTAG, WE SHOULD SKIP TO THE NEXT ITERATION
-                entrant_id = seed['entrantId']
-                this_player = seed['mutations']['players']
-                for player_id in this_player:
-                    id = player_id
+            try:
+                region = this_player[id]['region'].strip()
+            except Exception as regionEx:
+                print self.log('SmashGGPlayer ' + tag + ': region | ' + str(regionEx))
 
-                try:
-                    tag = this_player[id]['gamerTag'].strip()
-                except:
-                    print self.log('Player for id ' + str(id) + ' not found')
-                    continue
+            try:
+                state = this_player[id]['state'].strip()
+                if region is None:
+                    region = state
+            except Exception as stateEx:
+                print self.log('SmashGGPlayer ' + tag + ': state | ' + str(stateEx))
 
-                #EXTRACT EXTRA DATA FROM SMASHGG WE MAY WANT TO USE LATER
-                #ENCAPSULATE IN A SMASHGG SPECIFIC MODEL
-                try:
-                    name = this_player[id]['name'].strip()
-                except Exception as e:
-                    name = None
-                    print self.log('SmashGGPlayer ' + tag + ': name | ' + str(e))
-
-                try:
-                    region = this_player[id]['region'].strip()
-                except Exception as regionEx:
-                    print self.log('SmashGGPlayer ' + tag + ': region | ' + str(regionEx))
-
-                try:
-                    state = this_player[id]['state'].strip()
-                    if region is None:
-                        region = state
-                except Exception as stateEx:
-                    print self.log('SmashGGPlayer ' + tag + ': state | ' + str(stateEx))
-
-                try:
-                    country = this_player[id]['country'].strip()
-                    if region is None:
-                        region = country
-                except Exception as countryEx:
-                    print self.log('SmashGGPlayer ' + tag + ': country | ' + str(countryEx))
+            try:
+                country = this_player[id]['country'].strip()
+                if region is None:
+                    region = country
+            except Exception as countryEx:
+                print self.log('SmashGGPlayer ' + tag + ': country | ' + str(countryEx))
 
 
 
-                player = SmashGGPlayer(smashgg_id=id, entrant_id=entrant_id, name=name, smash_tag=tag, region=region,
-                                       state=state, country=country)
-                self.players.append(player)
-        except Exception as ex:
-            msg = 'An error occurred in the retrieval of SmashGG Players: ' + str(ex.message)
-            Log.log('SmashGG', msg)
+            player = SmashGGPlayer(smashgg_id=id, entrant_id=entrant_id, name=name, smash_tag=tag, region=region,
+                                   state=state, country=country)
+            self.players.append(player)
         return self.players
 
     def get_smashgg_matches(self):
@@ -191,29 +169,25 @@ class SmashGGScraper(object):
         like how far into the tournament or how many matches were played.
         """
         self.matches = []
-        try:
-            sets = self.get_raw()['smashgg']['entities']['sets']
-            for set in sets:
-                winner_id = set['winnerId']
-                loser_id = set['loserId']
-                # CHECK FOR A BYE
-                if loser_id is None:
-                    continue
+        sets = self.get_raw()['smashgg']['entities']['sets']
+        for set in sets:
+            winner_id = set['winnerId']
+            loser_id = set['loserId']
+            # CHECK FOR A BYE
+            if loser_id is None:
+                continue
 
-                try:
-                    name = set['fullRoundText']
-                    round = set['round']
-                    bestOf = set['bestOf']
-                except:
-                    print self.log('Could not find extra details for match')
-                    round = None
-                    bestOf = None
+            try:
+                name = set['fullRoundText']
+                round = set['round']
+                bestOf = set['bestOf']
+            except:
+                print self.log('Could not find extra details for match')
+                round = None
+                bestOf = None
 
-                match = SmashGGMatch(name, winner_id, loser_id, round, bestOf)
-                self.matches.append(match)
-        except Exception as ex:
-            msg = 'An error occurred in the retrieval of SmashGG Matches: ' + str(ex.message)
-            Log.log('SmashGG', msg)
+            match = SmashGGMatch(name, winner_id, loser_id, round, bestOf)
+            self.matches.append(match)
         return self.matches
 
     def _check_for_200(self, response):
@@ -221,11 +195,7 @@ class SmashGGScraper(object):
         :param response: http response to check for correct http code
         :return: the body response from a successful http call
         """
-        try:
-            response.raise_for_status()
-        except Exception as ex:
-            msg = 'An error occurred in the http request to SmashGG: ' + str(ex.message)
-            Log.log('SmashGG', msg)
+        response.raise_for_status()
         return response
 
     def log(self, msg):
@@ -242,11 +212,7 @@ class SmashGGScraper(object):
         :param url: url to parse the tournament id from
         :return: the unique id of the bracket in question
         """
-        try:
-            id = url[url.rfind('/') + 1:]
-        except Exception as ex:
-            msg = 'An error occurred in parsing out the SmashGG tournament id: ' + str(ex.message)
-            Log.log('SmashGG', msg)
+        id = url[url.rfind('/') + 1:]
         return int(id)
 
     @staticmethod
@@ -256,16 +222,10 @@ class SmashGGScraper(object):
         :param url: url to parse the tournament name from
         :return: the name of the tournament in question
         """
-        name = None
-        try:
-            tStr = 'tournament/'
-            startIndex = url.rfind(tStr) + len(tStr)
-            name = url[startIndex: url.index('/', startIndex)]
-            name = name.replace('-', ' ')
-        except Exception as ex:
-            msg = 'An error occurred in parsing out the SmashGG tournament name: ' + str(ex.message)
-            Log.log('SmashGG', msg)
-        return name
+        tStr = 'tournament/'
+        startIndex = url.rfind(tStr) + len(tStr)
+        name = url[startIndex: url.index('/', startIndex)]
+        return name.replace('-', ' ')
 
 class SmashGGPlayer(object):
     def __init__(self, smashgg_id, entrant_id, name, smash_tag, region, country, state):
