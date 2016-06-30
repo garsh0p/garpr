c1866900b1f989b447e50f5d5374c4a50008b0d9
Cooke, Brandon (bc719c) <bc719c@us.att.com>
2016-06-29 22:56:20 -0400

    Changed smashgg api url to include groups
    Functions to get raw groups and phaseid
    
    Recoded getting phase ids. Should correctly extract all pool id's that we can use to get the brackets.
    
    Quick bug fix
    
    Shifting things so it will keep working.
    
    Creating method to pull back pools into an array of SmashGG objects
    
    Successfully retrieving all phase id's from an event. Passing all tests.
    
    Successfully retrieves pools and stores them as a property in the SmashGGScraper object. Added tests. Passes all tests.
    
    Eradicated the infinite recursion (via object property).
    Need to check if double counting happens
    
    Added in condition to recursively pulling pools matches. Prevents double counting the matches we already accounted for in the url.
    
    Added Recursive Pools grabbing code to SmashGG match pulling.
    Altered tests to apply to recursive match pulling.

--- scraper/smashgg.py
+++ scraper/smashgg.py
@@ -4,12 +4,13 @@ import os
 from model import MatchResult
 from garprLogging.log import Log
 
-BASE_SMASHGG_API_URL = "https://api.smash.gg/phase_group/"
-TOURNAMENT_URL = os.path.join(BASE_SMASHGG_API_URL, '%s')
-DUMP_SETTINGS = "?expand[0]=sets&expand[1]=seeds&expand[2]=entrants&expand[3]=matches"
-
-
-
+### SMASHGG URLS: https://smash.gg/tournament/<tournament-name>/brackets/<event-id>/<group-id>/<phase-group-id>
+BASE_SMASHGG_EVENT_API_URL = "https://api.smash.gg/event/"
+BASE_SMASHGG_PHASE_API_URL = "https://api.smash.gg/phase_group/"
+EVENT_URL = os.path.join(BASE_SMASHGG_EVENT_API_URL, '%s')
+TOURNAMENT_URL = os.path.join(BASE_SMASHGG_PHASE_API_URL, '%s')
+DUMP_SETTINGS_GROUPS = "?expand[0]=groups"
+DUMP_SETTINGS_ALL = "?expand[0]=sets&expand[1]=entrants&expand[2]=matches&expand[3]=seeds"
 
 class SmashGGScraper(object):
     def __init__(self, path):
@@ -17,34 +18,42 @@ class SmashGGScraper(object):
         :param path: url to go to the bracket
         """
         self.path = path
-        self.tournament_id = SmashGGScraper.get_tournament_id_from_url(self.path)
+
+        #DETERMINES IF A SCRAPER IS FOR A POOL
+        #PREVENTS INFINITE RECURSION
+        self.is_pool = False
+
+        #GET IMPORTANT DATA FROM THE URL
+        self.tournament_id = SmashGGScraper.get_tournament_phase_id_from_url(self.path)
+        self.event_id = SmashGGScraper.get_tournament_event_id_from_url(self.path)
         self.name = SmashGGScraper.get_tournament_name_from_url(self.path)
 
+        #DEFINE OUR TARGET URL ENDPOINT FOR THE SMASHGG API
+        #AND INSTANTIATE THE DICTIONARY THAT HOLDS THE RAW
+        # JSON DUMPED FROM THE API
         base_url = TOURNAMENT_URL % self.tournament_id
-        self.apiurl = base_url + DUMP_SETTINGS
-
+        self.apiurl = base_url + DUMP_SETTINGS_ALL
         self.raw_dict = None
+
+        #DATA STRUCTURES THAT HOLD IMPORTANT THINGS
+        self.phase_ids = []
+        self.pools = []
         self.players = []
+
+        #GET THE RAW JSON AT THE END OF THE CONSTRUCTOR
         self.get_raw()
-        #we don't use a try/except block here, if something goes wrong, we *should* throw an exception
 
 ######### START OF SCRAPER API
-
 
     def get_raw(self):
         """
         :return: the JSON dump that the api call returns
         """
-        try:
-            if self.raw_dict == None:
-                self.raw_dict = {}
-                self.log('API Call to ' + str(self.apiurl) + ' executing')
-                self.raw_dict['smashgg'] = self._check_for_200(requests.get(self.apiurl)).json()
-            return self.raw_dict
-        except Exception as ex:
-            msg = 'An error occurred in the retrieval of data from SmashGG: ' + str(ex)
-            Log.log('SmashGG', msg)
-            return msg
+        if self.raw_dict == None:
+            self.raw_dict = {}
+            self.log('API Call to ' + str(self.apiurl) + ' executing')
+            self.raw_dict['smashgg'] = self._check_for_200(requests.get(self.apiurl)).json()
+        return self.raw_dict
 
     def get_name(self):
         return self.name
@@ -66,24 +75,36 @@ class SmashGGScraper(object):
         played in the given bracket, including who won and who lost
         """
         matches = []
-        try:
-            sets = self.get_raw()['smashgg']['entities']['sets']
-            for set in sets:
-                winner_id = set['winnerId']
-                loser_id = set['loserId']
-                # CHECK FOR A BYE
-                if loser_id is None:
+        sets = self.get_raw()['smashgg']['entities']['sets']
+        for set in sets:
+            winner_id = set['winnerId']
+            loser_id = set['loserId']
+            # CHECK FOR A BYE
+            if loser_id is None:
+                continue
+
+            winner = self.get_player_by_entrant_id(winner_id)
+            loser = self.get_player_by_entrant_id(loser_id)
+
+            match = MatchResult(winner.smash_tag, loser.smash_tag)
+            matches.append(match)
+
+        # RECURSIVELY DIG AND RETRIEVE MATCHES FROM OTHER PHASES
+        # WE ONLY WANT TO FETCH POOLS OF THE TOP BRACKET OTHERWISE
+        # WE GET CAUGHT IN INFINITE RECURSION
+        if self.is_pool is False:
+            if len(self.pools) is 0:
+                self.get_pools()
+            for pool in self.pools:
+                #CONDITION TO PREVENT DOUBLE COUNTING MATCHES
+                if pool.tournament_id == self.tournament_id:
                     continue
 
-                winner = self.get_player_by_entrant_id(winner_id)
-                loser = self.get_player_by_entrant_id(loser_id)
+                pool_matches = pool.get_matches()
+                for match in pool_matches:
+                    matches.append(match)
 
-                match = MatchResult(winner.smash_tag, loser.smash_tag)
-                matches.append(match)
-        except Exception as ex:
-            msg = 'An error occured in the retrieval of matches: ' + str(ex)
         return matches
-        #we dont try/except, we throw when we have an issue
 
 ####### END OF SCRAPER API
 
@@ -139,7 +160,7 @@ class SmashGGScraper(object):
             country         = None
             region          = None
             entrant_id      = None
-            smashgg_id       = None
+            smashgg_id      = None
             final_placement = None
 
             try:
@@ -202,7 +223,7 @@ class SmashGGScraper(object):
         than just the winner and loser. Could be useful for additional ranking metrics
         like how far into the tournament or how many matches were played.
         """
-        self.matches = []
+        matches = []
         sets = self.get_raw()['smashgg']['entities']['sets']
         for set in sets:
             winner_id = set['winnerId']
@@ -221,8 +242,45 @@ class SmashGGScraper(object):
                 bestOf = None
 
             match = SmashGGMatch(name, winner_id, loser_id, round, bestOf)
-            self.matches.append(match)
-        return self.matches
+            matches.append(match)
+        # RECURSIVELY DIG AND RETRIEVE MATCHES FROM OTHER PHASES
+        # WE ONLY WANT TO FETCH POOLS OF THE TOP BRACKET OTHERWISE
+        # WE GET CAUGHT IN INFINITE RECURSION
+        if self.is_pool is False:
+            if len(self.pools) is 0:
+                self.get_pools()
+            for pool in self.pools:
+                # CONDITION TO PREVENT DOUBLE COUNTING MATCHES
+                if pool.tournament_id == self.tournament_id:
+                    continue
+
+                pool_matches = pool.get_smashgg_matches()
+                for match in pool_matches:
+                    matches.append(match)
+        return matches
+
+    def get_pool_by_phase_id(self, id):
+        if id is None:
+            pass
+        pool_url = BASE_SMASHGG_PHASE_API_URL + str(id)
+        pool = SmashGGScraper(pool_url)
+        pool.is_pool = True
+        self.pools.append(pool)
+
+    def get_pools(self):
+        if self.raw_dict is None:
+            self.raw_dict = self.get_raw()
+        if len(self.phase_ids) is 0:
+            self.phase_ids = self.get_phase_ids()
+
+        for id in self.phase_ids:
+            if id is None:
+                continue
+            pool_url = BASE_SMASHGG_PHASE_API_URL + str(id)
+            pool = SmashGGScraper(pool_url)
+            pool.is_pool = True
+            self.pools.append(pool)
+        return self.pools
 
     def _check_for_200(self, response):
         """
@@ -239,8 +297,46 @@ class SmashGGScraper(object):
         """
         return "    [SmashGG] " + msg
 
+    def remove_list_repeats(self, list):
+        temp = []
+        for item in list:
+            if item not in temp:
+                temp.append(item)
+        return temp
+
+    def get_groups(self, raw):
+        groups = raw['entities']['groups']
+        return groups
+
+    def get_phase_ids(self):
+        phase_ids = []
+        event_id = SmashGGScraper.get_tournament_event_id_from_url(self.path)
+        event_url = BASE_SMASHGG_EVENT_API_URL + str(event_id) + DUMP_SETTINGS_GROUPS
+
+        event_raw = self._check_for_200(requests.get(event_url)).json()
+        groups = self.get_groups(event_raw)
+        for group in groups:
+            #EACH ID REPRESENTS A POOL
+            phase_ids.append(str(group['id']).strip())
+        return self.remove_list_repeats(phase_ids)
+
     @staticmethod
-    def get_tournament_id_from_url(url):
+    def get_tournament_event_id_from_url(url):
+        splits = url.split('/')
+
+        flag = False
+        for split in splits:
+            #IF THIS IS TRUE WE HAVE REACHED THE EVENT ID
+            if flag is True:
+                return int(split)
+
+            #SET FLAG TRUE IF CURRENT WORD IS 'BRACKETS'
+            #THE NEXT ELEMENT WILL BE OUR EVENT ID
+            if 'brackets' in split:
+                flag = True
+
+    @staticmethod
+    def get_tournament_phase_id_from_url(url):
         """
         Parses a url and retrieves the unique id of the bracket in question
         :param url: url to parse the tournament id from
