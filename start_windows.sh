5e2529b577abf6e78f0eed6d4809248c48727831
Cooke, Brandon (bc719c) <bc719c@us.att.com>
2016-08-20 18:05:23 -0400

    Added start script with Windows ending lines. Any Windows developer should run this file instead of start.sh

---
+++ start_windows.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+source config/config.ini
+if [[ "$(ps aux | grep mongo)" == *"mongod"* ]]
+	then
+		echo "mongod is already running"
+	else
+		echo "starting mongo"
+		mongod &
+fi
+if [[ "$(ps aux | grep server.py)" == *"python server.py"* ]]
+	then
+		echo "backend is already running"
+	else
+		echo "starting backend"
+		python server.py $api_port True &
+fi
+if [[ "$(ps aux | grep SimpleHTTPServer)" == *"python -m SimpleHTTPServer"* ]]
+	then
+		echo "frontend is already running"
+	else
+		echo "starting frontend"
+		pushd webapp; python -m SimpleHTTPServer $web_port; popd &
+fi
+echo "everything started, try http://localhost:$web_port"
